452d8134a6df4c891627808f300b13fd3dfafe47
Mihai-Valeriu Dobra <mvdobra@icloud.com>
2024-04-20 15:10:18 +0300

    vscCommit#1Update

--- linkedinLoginTest.cy.js
+++ linkedinLoginTest.cy.js
@@ -4,18 +4,28 @@ describe('linkedinLoginTest', () => {
       // Access the www.linkedin.com websote.
       cy.visit('https://www.linkedin.com/');
 
+      cy.wait(2000);
+
       // Accept cookies
       cy.get('[action-type="ACCEPT"]').click();
+
+      cy.wait(1000);
 
       // Access the login page
       cy.get('.nav__button-secondary').click();
 
+      cy.wait(1000);
+
       // Insert the credentials
-      cy.get('#username').type('yourEmail');
-      cy.get('#password').type('yourPassword');
+      cy.get('#username').type('typeEmail');
+      cy.get('#password').type('typePassword');
+
+      cy.wait(1000);
 
       // Access the login button
       cy.get('.btn__primary--large').click();
+
+      cy.wait(1000);
 
       // Assertion to ensure successful login
       cy.url().should('include', '/feed');
